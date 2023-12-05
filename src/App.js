018428524dffbced5091515df5518edd23e0ca61
Maharaj7809 <134138887+Maharaj7809@users.noreply.github.com>
2023-12-05 16:49:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,33 @@ import React, { useState,useEffect, useRef } from 'react';
 
 const App = () => {
   const searchRef = useRef(null);
+const [move, setMove] = useState([]);
+ 
+ const TMDB_API_KEY = 'fe659372fbb274dea4b77b83b77b8663';
+ 
+ const getItem=async()=>{
+   const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}`;
+   try {
+     const response = await axios.get(apiUrl);
+     const popularMovies = response.data.results;
+     setMove(popularMovies);
+     console.log(popularMovies);
+   } catch (error) {
+     console.error('An error occurred:', error);
+   }
+ }
+ 
+   useEffect(() => {
+     getItem();
+ 
+   }, []); 
 
   return (
     <div className="app">
     <BrowserRouter>
       <Navbar searchRef={searchRef}/>
       <Routes>
-        <Route path="/" element={<Home searchRef={searchRef} />} />
+        <Route path="/" element={<Home searchRef={searchRef}  move={move}/>} />
       </Routes>
     </BrowserRouter>
   </div>
