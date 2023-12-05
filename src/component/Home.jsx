912be65c54a24e22d07be5d3849c32d4bf91e89f
Maharaj7809 <134138887+Maharaj7809@users.noreply.github.com>
2023-12-05 17:34:37 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -28,7 +28,7 @@ let slides = [];
  
   return (
     <div>
-     <div style={{ height:'50vh' }}>  {isNonMobile?  <Carousel slides={slides} autoplay={true} interval={20000} /> : <Carosel/>} </div>
+     <div style={{ height:'45vh' }}>  {isNonMobile?  <Carousel slides={slides} autoplay={true} interval={20000} /> : <Carosel/>} </div>
     
       <Search searchRef={searchRef}/>
     
