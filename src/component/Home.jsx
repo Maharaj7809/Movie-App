bb8c4c3cb48ec51bd925cb6572db18c3e4e3d9a4
Maharaj7809 <134138887+Maharaj7809@users.noreply.github.com>
2023-12-05 16:52:42 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,13 +3,31 @@ import Carosel from "./Carosel"
 import {  useMediaQuery } from "@mui/material";
 import Footer from './Footer';
 import Search  from './Search';
+import {Carousel} from '3d-react-carousal';
+import axios from 'axios';
 
-const Home = ({ searchRef }) => {
+const Home = ({ searchRef,move }) => {
+
+let slides = [];
+
+ if (move) {
+   slides = move.map((movi, indx) => (
+     <iframe
+       key={`iframe-${indx}`}
+       src={`https://vidsrc.in/embed/movie?tmdb=${movi.id}`}
+       frameBorder="0"
+       allowFullScreen
+       width={460}
+       height={270}
+     ></iframe>
+   ));
+ }
+ 
   
   const isNonMobile = useMediaQuery("(min-width:1200px)");
   return (
     <div>
-      <Carosel/>
+      (isNonMobile? <Carosel/>: <Carousel slides={slides} autoplay={true} interval={20000} />)
       <Search searchRef={searchRef}/>
     
     </div>
